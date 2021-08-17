7661d105be3cebd659f2f8339919484e667f496c
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-08-17 17:24:11 +0200

    Backup;Decoder_DS18B20

--- Program/com_driver.py
+++ Program/com_driver.py
@@ -4,24 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import keyboard
 
-#serialPort = serial.Serial(port="COM4", baudrate=9600,bytesize = 8, 
-#                             timeout = None, stopbits=serial.STOPBITS_ONE)
+def show():
+    y = []
+    with open("archive.bin", "rb") as file:
+        bytes_buffer = file.read(18)
+        while(bytes_buffer != ''.encode('utf-8')):
+            sr_buffer = [float(val) for val in bytes_buffer.split()]
+            y.append(sr_buffer)
+            bytes_buffer = file.read(18)
+    plt.plot(np.array(y), '-p')
+            
 
 with serial.Serial(port="COM4", baudrate=9600,bytesize = 8, 
                    timeout = None, stopbits=serial.STOPBITS_ONE) as serialPort:
-
     y = []
-    
-    time.sleep(5)
-    
-    while(keyboard.read_key()!='q'):
-        serialPort.write('tempval\n'.encode('utf-8'))
-        temp_buff = serialPort.readline().strip(' \n'.encode('utf-8'))
-        single_read_buff = []
-        for val in temp_buff.split():
-            val = float(val)
-            single_read_buff.append(val)
-        y.append(single_read_buff)
-        
-    plt.plot(np.array(y), '.')
-    
+    time.sleep(2)
+    with open("archive.bin", "ab") as file:
+        while(keyboard.read_key()!='q'):
+            serialPort.write('tempval\n'.encode('utf-8'))
+            temp_buff = serialPort.readline().strip('\n'.encode('utf-8'))
+            file.write(temp_buff)
+            single_read_buff = [float(val) for val in temp_buff.split()]
+            y.append(single_read_buff)        
+    plt.plot(np.array(y), '-p')
+    show()
