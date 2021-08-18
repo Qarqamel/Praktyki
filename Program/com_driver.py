f12f74b64bf58e849f6b927db6f1f998cac64209
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-08-18 20:25:54 +0200

    Backup;Decoder_DS18B20

--- Program/com_driver.py
+++ Program/com_driver.py
@@ -15,8 +15,8 @@ with serial.Serial(port="COM4", baudrate=9600,bytesize = 8,
     while(keyboard.read_key()!='q'):
         serialPort.write('tempval\n'.encode('utf-8'))
         temp_buff = serialPort.readline().strip('\n'.encode('utf-8'))
-        single_read_buff = [float(val) for val in temp_buff.split()]
+        single_read_buff = list(map(float, temp_buff.split()))
+        print(single_read_buff)
         y.append(single_read_buff)
 np.save('archive.npy', np.array(y))
-plt.plot(np.array(y), '-p')
 show()
