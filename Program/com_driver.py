9cee4dbb31597d589b4889efd74ff5ef868ca919
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-08-18 20:13:17 +0200

    Backup;Decoder_DS18B20

--- Program/com_driver.py
+++ Program/com_driver.py
@@ -5,12 +5,8 @@ import numpy as np
 import keyboard
 
 def show():
-    with open("archive.bin", "rb") as file:
-        read_arr = np.fromfile(file, dtype = np.dtype([('temp1', 'float64'),
-                                              ('temp2', 'float64'),
-                                              ('temp3', 'float64')]))
-    new_arr = [list(elem) for elem in read_arr]
-    plt.plot(np.array(new_arr), '-p')         
+    loaded_arr = np.load('archive.npy')
+    plt.plot(np.array(loaded_arr), '-p')         
 
 with serial.Serial(port="COM4", baudrate=9600,bytesize = 8, 
                    timeout = None, stopbits=serial.STOPBITS_ONE) as serialPort:
@@ -21,7 +17,6 @@ with serial.Serial(port="COM4", baudrate=9600,bytesize = 8,
         temp_buff = serialPort.readline().strip('\n'.encode('utf-8'))
         single_read_buff = [float(val) for val in temp_buff.split()]
         y.append(single_read_buff)
-with open("archive.bin", "ab") as file:
-    np.array(y).tofile(file)
+np.save('archive.npy', np.array(y))
 plt.plot(np.array(y), '-p')
 show()
