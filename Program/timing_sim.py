5051ec10e994a43ad97d70dfa8d132c8b39454b7
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-08-27 17:13:07 +0200

    Backup;DS18B20_chain

--- Program/timing_sim.py
+++ Program/timing_sim.py
@@ -1,22 +1,35 @@
 nr_of_devices = 32
-id_bytes = 6
+id_length_bytes = 6
+id_tx_bytes = id_length_bytes*2 + 2
 temp_bytes = 5
-single_measure_bytes = id_bytes + temp_bytes + 3
+single_measure_bytes = temp_bytes + 2
 baud_rate = 9600
 stop_bit = 1
 byte_tx_time_ms = (8 + stop_bit)*1000/baud_rate
 max_temp_conversion_time_ms = 93.75
 
+single_measure_tx_time_ms = byte_tx_time_ms*single_measure_bytes
+
+print("Single measure format: ;30.00")
+print("Transmission of single measure takes " + str(single_measure_tx_time_ms) + " ms")
+
+single_id_tx_time_ms = byte_tx_time_ms*id_tx_bytes
+
+print("Single id format: ;0123456789ab")
+print("Transmission of single id takes " + str(single_id_tx_time_ms) + " ms")
+
 execution_time = 0
 
 for dev_nr in range(nr_of_devices):
-    execution_time += byte_tx_time_ms*single_measure_bytes*(dev_nr+1) + max_temp_conversion_time_ms
+    execution_time += single_measure_tx_time_ms*(dev_nr+1)
     
 print("Execution with temp measurements takes " + str(execution_time) + " ms")
 
 execution_time = 0
 
 for dev_nr in range(nr_of_devices):
-    execution_time += byte_tx_time_ms*(single_measure_bytes - temp_bytes - 1)*(dev_nr+1)
+    execution_time += single_id_tx_time_ms*(dev_nr+1)
     
-print("Execution time without temp measurements takes " + str(execution_time) + " ms")
+print("Execution with id's takes " + str(execution_time) + " ms")
+
+print()
