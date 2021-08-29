60918d871d36f7a67a31a00cc05809e6b830a71f
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-08-29 16:53:19 +0200

    Backup;Echo_temp_no_buffer

--- Program/Program.ino
+++ Program/Program.ino
@@ -1,21 +1,25 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ID_BYTES 8
+//#define ID_BYTES 8
 
 OneWire oneWire(A0);
 DallasTemperature sensor(&oneWire);
-char id_string_buffer[ID_BYTES*2];
+//char id_string_buffer[ID_BYTES*2+1];
+float temp_buffer;
 
 void setup() {
-  unsigned char therm_id[ID_BYTES];
+//  unsigned char therm_id[ID_BYTES];
       
-  sensor.begin();  
+  sensor.begin();
+  sensor.setResolution(9);  
   Serial.begin(9600);
   Serial.setTimeout(-1);
 
-  sensor.getAddress(therm_id, 0);
-  byte_array_to_hex_string(6, therm_id+1, id_string_buffer);
+//  sensor.getAddress(therm_id, 0);
+//  byte_array_to_hex_string(6, therm_id+1, id_string_buffer);
+  sensor.requestTemperatures();
+  temp_buffer = sensor.getTempCByIndex(0);
 }
 
 void loop (){
@@ -27,14 +31,18 @@ void loop (){
     Serial.print(Rcvd_char[0]);
   }
   else{
+//    Serial.print(";");
+//    Serial.println(id_string_buffer);
     Serial.print(";");
-    Serial.println(id_string_buffer);
+    Serial.println(temp_buffer);
+    sensor.requestTemperatures();
+    temp_buffer = sensor.getTempCByIndex(0);
   }
 }
 
-void byte_array_to_hex_string(unsigned char byte_array_size, unsigned char byte_array[], char hex_string[]){
-  for(unsigned char i = 0; i<byte_array_size; i++){
-    sprintf(hex_string+(2*i), "%.2X", byte_array[i]);
-  }
-  hex_string[byte_array_size*2] = NULL;
-}
+//void byte_array_to_hex_string(unsigned char byte_array_size, unsigned char byte_array[], char hex_string[]){
+//  for(unsigned char i = 0; i<byte_array_size; i++){
+//    sprintf(hex_string+(2*i), "%.2X", byte_array[i]);
+//  }
+//  hex_string[byte_array_size*2] = NULL;
+//}
